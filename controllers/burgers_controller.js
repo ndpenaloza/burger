b1233c4ab2fce54c7a776213a4f1d56273e4b0a6
NDPenaloza <ndpenaloza@gmail.com>
2021-01-08 12:12:45 -0800

    wrote more burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,5 +12,40 @@ router.get("/", (req, res) => {
         console.log(burgerObject);
         res.render("index", burgerObject);
     });
+
+    router.post("/api/burgers", (req, res) => {
+        burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) => {
+            res.json({id: result.insertID});
+        } 
+        )
+    });
+
+    router.put("/api/burgers/:id", (req, res) => {
+        let condition = "id = " + req.params.id;
+        console.log("condition", condition);
+        
+        burger.updateOne({ devoured: req.body.devoured}, condtion, (result) => {
+            if ((result, changedRows === 0)) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+    router.deleteOne(condition, (req, res) => {
+        let condition = "id = " + req.params.id;
+        console.log("condition", condition);
+
+        burger.deleteOne(condition, (result) => {
+            if ((result, changedRows === 0)) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        })
+    })
 });
 
+
+module.exports = router;
