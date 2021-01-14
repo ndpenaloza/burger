7d3aec4750f3bb0a4243dcaa3a58a060312ca639
NDPenaloza <ndpenaloza@gmail.com>
2021-01-13 22:43:52 -0800

    added jawsdb to connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@ const mysql = require("mysql");
 var password = require("./password");
 
 // Connection to MySQL 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: password,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password,
+    database: "burgers_db"
+  });
+};
+
 
 // Connecting to MySQL
 connection.connect((err) => {
